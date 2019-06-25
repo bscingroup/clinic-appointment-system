b02e22c546a86a328926f549845932a55836a681
ibrahimgoodluck <50639747+ibrahimgoodluck@users.noreply.github.com>
2019-06-25 13:05:42 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>page about</title>
+	<style type="text/css">
+		p{
+			font-size: 30px;
+			box-shadow: 6px 6px 8px 9px gray;
+			border-radius:60px;
+
+		}
+		
+		ul{
+				list-style-type: none;
+			font-size: 35px;
+
+		}
+		
+			.h{
+			
+				position: absolute;
+				top: 370px; 
+				
+			}
+
+	</style>
+</head>
+<body style="background-color:magenta;border-radius:60px">
+	<div class="h">
+	 <ul>
+       <li><a href="index.php">Home</a></li>
+        <li><a href="second.php">Register</a></li>
+      </ul>
+</div>
+
+<center><p>This project was prepared  by  <b>Rose,Selegebu,Ibrahim and Mwanahamisi:</b><br><br>
+ We are the first year  students at the university of Dar es salam:<br> <br>Taking Bsc in computer science in 2018/2019.<br><br>Under Mr Bernard Musa as our instructor of WEB PROGRAMMING (IS 181) and sir Michael as our Practical assistance. <br><br>We are appreciate our instructor and our Practical assistance with their advices to assist us to acomplish this work  </p></center>
+ <center><img src="hosp3.jpg" width="350px" height="400px">
+ <img src="hosp1.jpg" width="350px" height="400px">
+<img src="hosp1.jpg" width="350px" height="400px">
+<img src="hosp3.jpg" width="350px" height="400px">
+
+</center>
+<center>
+<br><br>
+
+<footer style="font-size: 25px"><ins>
+				&copyClinic appointment system 2019:
+			</ins></footer></center>
+</body>
+</html>
